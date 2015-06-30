3e37ea3b6ff00c7d493add8b0236583dd0bd5159
Nico <nico_florin@gmx.ch>
2015-06-30 20:16:05 +0200

    init.php integriert

--- public/index.php
+++ public/index.php
@@ -4,7 +4,16 @@
  * Alle Zugriffe werden über diese Datei geroutet.
  */
 define('ROOT', realpath(dirname(__DIR__))); // realpath gibt eindeutigen und absoluten Pfadnamen zurück
-require_once (ROOT . '/app/init.php');
+
+//Library
+require_once (ROOT . '/app/libs/App.php');
+require_once (ROOT . '/app/libs/Controller.php');
+require_once (ROOT . '/app/libs/Database.php');
+require_once (ROOT . '/app/libs/Model.php');
+require_once (ROOT . '/app/libs/View.php');
+
+//Configs
+require_once (ROOT . '/app/configs/paths.php');
 
 // Starte Hauptscript
 $app = new App();
