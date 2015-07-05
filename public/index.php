990ae5ba902d2271e53457dab7ecef5da153a02e
Nico <nico_florin@gmx.ch>
2015-07-05 17:23:53 +0200

    Session include hinzugefügt

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ require_once (ROOT . '/app/libs/App.php');
 require_once (ROOT . '/app/libs/Controller.php');
 require_once (ROOT . '/app/libs/Database.php');
 require_once (ROOT . '/app/libs/Model.php');
+require_once (ROOT . '/app/libs/Session.php');
 require_once (ROOT . '/app/libs/View.php');
 
 //Configs
