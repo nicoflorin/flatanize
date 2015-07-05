7d12f6d91499038611e27aec39ab8f7ec0732478
Nico <nico_florin@gmx.ch>
2015-07-05 18:12:14 +0200

    Intial erstellt

---
+++ app/views/app/left_nav.php
@@ -0,0 +1,10 @@
+<div class="col-md-3 col-xs-12">
+    <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="<?= URL ?>">Home</a></li>
+        <li><a href="#">Shopping List</a></li>
+        <li><a href="#">Cleaning Schedules</a></li>
+        <li><a href="#">Financials</a></li>
+        <li><hr /></li>
+        <li><a href="#">Settings</a></li>
+    </ul>
+</div>
