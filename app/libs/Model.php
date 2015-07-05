648eab98eb4ca1c1cdba05862ffa2a5abcbc3db8
Nico <nico_florin@gmx.ch>
2015-07-05 16:09:43 +0200

    try catch um new DB()

--- app/libs/Model.php
+++ app/libs/Model.php
@@ -4,10 +4,15 @@
  * Model Hauptklasse
  */
 class Model {
+
     protected $db;
 
     function __construct() {
-        $this->db = new Database();
+        try {
+            $this->db = new Database();
+        } catch (Exception $e) {
+            echo 'Database error: ' . $e->getMessage();
+        }
     }
 
 }
