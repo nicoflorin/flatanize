13b5eb5972fd28b3a449091405d8c10746e0804a
Nico <nico_florin@gmx.ch>
2015-07-05 18:12:20 +0200

    Intial erstellt

---
+++ app/views/app/index.php
@@ -0,0 +1,12 @@
+<div class="well">
+    <div class="page-header">
+        <h1>Home</h1>
+    </div>
+    <div class="row">
+        <?php require_once 'left_nav.php'; ?>
+
+        <div class="col-md-9 col-xs-12">
+            
+        </div>
+    </div><!-- end row -->
+</div>
