1e77e7928bc1c304f88f04f873cd735d63002a9b
Nico <nico_florin@gmx.ch>
2015-07-05 16:00:33 +0200

    User already exists check hinzugefügt

--- app/models/RegisterModel.php
+++ app/models/RegisterModel.php
@@ -13,9 +13,8 @@ class Register extends Model {
      * Prüft Benutzer Eingaben und erstellt den User in der DB.
      * @param array $request
      */
-    public function register (array $request) {
+    public function register(array $request) {
         // @Todo doppelte User prüfen
-        
         //input sent from post
         $in_userName = $request['username'];
         $in_displayName = $request['displayname'];
@@ -25,23 +24,40 @@ class Register extends Model {
 
         $error = [];
         if (empty($in_userName)) {
-            $error['username'] = "false";
+            $error['username'] = true;
         }
         if (empty($in_displayName)) {
-            $error['displayname'] = "false";
+            $error['displayname'] = true;
         }
         if (empty($in_email)) {
-            $error['email'] = "false";
+            $error['email'] = true;
         }
         if (empty($in_password)) {
-            $error['password'] = "false";
+            $error['password'] = true;
         }
-        //falls Fehler aufgetreten sind, $error Array zurückgeben
-        if (!empty($error)) {
+
+        // Prüfe ob User oder email Adresse bereits existiert
+        $bind = array(
+            ':username' => $in_userName,
+            ':email' => $in_email
+        );
+        $count = $this->db->select('count(*) as cnt', 'users', 'username = :username OR email = :email', $bind);
+        if ($count[0]['cnt'] > 0) {
+            $error['dupl_user'] = true;
             
+        }
+
+        //falls Eingabe Fehler aufgetreten sind, $error Array zurückgeben
+        if (!empty($error)) {
+            if (isset($error['dupl_user'])) {
+                $error['error_msg'] = 'Username or Email Adress already exists! Please choose another one.';
+            }else {
+                $error['error_msg'] = 'Please provide all required informations!';
+            }
+
             // Korrekt Befüllte Felder wieder zurückgeben
             if (!empty($in_userName)) {
-               $error["username"] = $in_userName;
+                $error["username"] = $in_userName;
             }
             if (!empty($in_displayName)) {
                 $error["displayname"] = $in_displayName;
@@ -55,7 +71,7 @@ class Register extends Model {
         } else { // sonst User erstellen
             $salt = openssl_random_pseudo_bytes(64); //Generiere Random String
             $hashed_password = hash_hmac("sha256", $in_password, $salt); // Hashe Passwort mit Salt
-                        
+
             $bind = array(
                 ':username' => $in_userName,
                 ':display_name' => $in_displayName,
@@ -69,15 +85,15 @@ class Register extends Model {
             // Falls ein Flat Code eingegeben wurde, den User gleich mit dieser WG verlinken
             if (!empty($in_flat_code)) {
                 /*
-                $stmt = $this->db->prepare("SELECT id FROM flats WHERE code = :flat_code");
-                $stmt->execute(array(':flat_code' => $in_flat_code));
-                $res = $stmt->fetch(PDO::FETCH_ASSOC);
-                */
+                  $stmt = $this->db->prepare("SELECT id FROM flats WHERE code = :flat_code");
+                  $stmt->execute(array(':flat_code' => $in_flat_code));
+                  $res = $stmt->fetch(PDO::FETCH_ASSOC);
+                 */
 
                 $bind = array(':flat_code' => $in_flat_code);
                 $res = $this->db->select('id', 'flats', 'code = :flat_code', $bind);
                 $id = $res[0]['id'];
-                
+
                 $bind = array(
                     ':id' => $id,
                     ':username' => $in_userName
--- app/views/register/signUp.php
+++ app/views/register/signUp.php
@@ -7,26 +7,26 @@
         <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
         <?php 
         if (isset($this->data['error'])) {
-            echo '<div class="alert alert-danger" role="alert"><p>Please provide all required information!</p></div>';
+            echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
         }
         ?>
         <form role="form" action="<?= URL ?>register/run" method="post">
             <div class="row">
                 <div class="col-md-6">
-                    <div class="form-group <?php echo (isset($this->data['username'])) ? 'has-error' : '' ?>">
-                        <input type="text" value = "<?php echo (isset($this->data['username']) && $this->data['username'] != "false") ? $this->data['username'] : '' ?>" name="username" id="first_name" class="form-control input-lg" placeholder="User Name" tabindex="1">
+                    <div class="form-group <?php echo ($this->data['username'] === true ) ? 'has-error' : '' ?>">
+                        <input type="text" value = "<?php echo (isset($this->data['username']) && $this->data['username'] !== true) ? $this->data['username'] : '' ?>" name="username" id="first_name" class="form-control input-lg" placeholder="User Name" tabindex="1">
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="form-group <?php echo (isset($this->data['displayname'])) ? 'has-error' : '' ?>">
-                        <input type="text" value = "<?php echo (isset($this->data['displayname']) && $this->data['displayname'] != "false") ? $this->data['displayname'] : '' ?>" name="displayname" id="last_name" class="form-control input-lg" placeholder="Display Name" tabindex="2">
+                    <div class="form-group <?php echo ($this->data['displayname']  === true ) ? 'has-error' : '' ?>">
+                        <input type="text" value = "<?php echo (isset($this->data['displayname']) && $this->data['displayname'] !== true) ? $this->data['displayname'] : '' ?>" name="displayname" id="last_name" class="form-control input-lg" placeholder="Display Name" tabindex="2">
                     </div>
                 </div>
             </div><!-- end row -->
-            <div class="form-group <?php echo (isset($this->data['email'])) ? 'has-error' : '' ?>">
-                <input type="email" value = "<?php echo (isset($this->data['email']) && $this->data['email'] != "false") ? $this->data['email'] : '' ?>" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="4">
+            <div class="form-group <?php echo ($this->data['email'] === true ) ? 'has-error' : '' ?>">
+                <input type="email" value = "<?php echo (isset($this->data['email']) && $this->data['email'] !== true) ? $this->data['email'] : '' ?>" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="4">
             </div>
-            <div class="form-group <?php echo (isset($this->data['password'])) ? 'has-error' : '' ?>">
+            <div class="form-group <?php echo ($this->data['password']  === true ) ? 'has-error' : '' ?>">
                 <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="5">
             </div>
             <div class="form-group">
