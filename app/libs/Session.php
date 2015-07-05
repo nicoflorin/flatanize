901994d2d5ce9567d5ca89db28655a181c1eb395
Nico <nico_florin@gmx.ch>
2015-07-05 17:24:41 +0200

    Initial Session Class erstellt

---
+++ app/libs/Session.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Description of Session
+ *
+ * @author Nico
+ */
+class Session {
+
+    public static function init() {
+        @session_start();
+    }
+
+    public static function set($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key) {
+        if (isset($_SESSION[$key])) {
+            return $_SESSION[$key];
+        } else {
+            return false;
+        }
+    }
+
+    public static function destroy() {
+        unset($_SESSION);
+        session_destroy();
+    }
+}
