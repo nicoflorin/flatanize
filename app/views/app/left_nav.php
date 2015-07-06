d9df8824dc5b45bb568ed0e38a9bdf66d215d10e
Nico <nico_florin@gmx.ch>
2015-07-06 19:15:44 +0200

    URLs angepasst

--- app/views/app/left_nav.php
+++ app/views/app/left_nav.php
@@ -1,6 +1,6 @@
     <ul class="nav nav-pills nav-stacked">
         <li class="<?php echo ($this->data['title'] == 'Home') ? 'active' : '' ?>"><a href="<?= URL ?>">Home</a></li>
-        <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>"><a href="#">Shopping List</a></li>
+        <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>"><a href="<?= URL ?>/app/shopping">Shopping List</a></li>
         <li class="<?php echo ($this->data['title'] == 'Cleaning Schedules') ? 'active' : '' ?>"><a href="#">Cleaning Schedules</a></li>
         <li class="<?php echo ($this->data['title'] == 'Financials') ? 'active' : '' ?>"><a href="#">Financials</a></li>
         <li class="<?php echo ($this->data['title'] == 'Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/app/settings">Settings</a></li>
