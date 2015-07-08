76d46665ab0bd3aa85fab6bd26d3f742e0af5181
Nico <nico_florin@gmx.ch>
2015-07-08 18:02:40 +0200

    Initial Cleaning

---
+++ app/models/cleaningModel.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Description of cleaningModel
+ *
+ * @author Nico
+ */
+class cleaningModel extends Model{
+    
+    /**
+     * Erstellt einen neuen Cleaning Task
+     */
+    public function create($title, $freq, $wday, $start) {
+        
+    }
+}
--- app/views/cleaning/create_task.php
+++ app/views/cleaning/create_task.php
@@ -9,7 +9,47 @@
                 <h1><?= $this->data['title'] ?></h1>
             </div>
 
-            
+            <!-- Create a Task -->
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Create a new task</h3>
+                </div>
+                <div class="panel-body">
+                    <form method="post" action="<?= URL ?>/cleaning/createTask">
+                        <div class="form-group">
+                            <input type="text" id="title" name="title" class="form-control" placeholder="Title" tabindex="1" required>
+                        </div>
+                        <div class="form-group">
+                            <!-- @todo auswahl per button -->
+                            <select name="frequency" class="form-control" tabindex="2" required> 
+                                <option value="" disabled selected>Select frequency</option>
+                                <option value="once">once</option>
+                                <option value="daily">daily</option>
+                                <option value="weekly">weekly</option>
+                                <option value="monthly">every month</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <select name="weekday" class="form-control" tabindex="3" placeholder="bla" required> 
+                                <option value="" disabled selected>Select weekday</option>
+                                <option value="mo">Monday</option>
+                                <option value="tu">Tuesday</option>
+                                <option value="we">Wednesday</option>
+                                <option value="th">Thursday</option>
+                                <option value="fr">Friday</option>
+                                <option value="sa">Saturday</option>
+                                <option value="su">Sunday</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="date" id="start" name="start" class="form-control" placeholder="Start" tabindex="4" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" value="Create Task" class="btn btn-success btn-block btn-lg" tabindex="5">
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div><!-- end well -->
     </div><!-- end col -->
 </div><!-- end row -->
