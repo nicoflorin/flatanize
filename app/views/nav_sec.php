4e5f11e49ab080d5a0670678d50e5e08aaec13ed
Nico <nico_florin@gmx.ch>
2015-07-08 16:43:28 +0200

    User -> Your Account

--- app/views/nav_sec.php
+++ app/views/nav_sec.php
@@ -25,7 +25,7 @@
             </ul><!-- end navbar-nav -->
             <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> User <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Your Account <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li><a href="<?= URL ?>/app/settings"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
                         <li class="divider"></li>
