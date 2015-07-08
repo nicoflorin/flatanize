ecd2cb542d0b9b833c55d90c82413cfaa509c929
Nico <nico_florin@gmx.ch>
2015-07-08 20:05:08 +0200

    divider hinzugefügt

--- app/views/app/left_nav.php
+++ app/views/app/left_nav.php
@@ -3,5 +3,6 @@
         <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>"><a href="<?= URL ?>/shopping/index">Shopping List</a></li>
         <li class="<?php echo ($this->data['title'] == 'Cleaning Tasks') ? 'active' : '' ?>"><a href="<?= URL ?>/cleaning/index">Cleaning Tasks</a></li>
         <li class="<?php echo ($this->data['title'] == 'Financials') ? 'active' : '' ?>"><a href="#">Finances</a></li>
+        <li class="nav-divider"></li>
         <li class="<?php echo ($this->data['title'] == 'Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/index">Settings</a></li>
     </ul>
