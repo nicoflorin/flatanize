3ff1575a23eb57c8a1d56282f24e0eb89f89cebf
Nico <nico_florin@gmx.ch>
2015-07-08 14:35:20 +0200

    FlatInfo Initial / shareFlat Initial

--- app/views/app/settings.php
+++ app/views/app/settings.php
@@ -8,6 +8,42 @@
             <div class="page-header">
                 <h1><?= $this->data['title'] ?></h1>
             </div>
+
+            <!-- Flat Infos -->
+            <div class="panel panel-primary" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
+                <div class="panel-heading">
+                    <h3 class="panel-title"><?= $this->flatName ?></h3>
+                </div>
+                <div class="panel-body">
+                    <p>Your Flat Code is: <?= $this->flatCode ?></p>
+                </div><!-- end panel-body -->
+            </div><!-- end flat infos -->
+
+            <!-- Share Flat -->
+            <div class="panel panel-primary" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
+                <div class="panel-heading">
+                    <h3 class="panel-title">Share Flat</h3>
+                </div>
+                <div class="panel-body">
+                    <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
+                    <?php
+                    if (isset($this->data['error_share'])) {
+                        echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
+                    }
+                    ?>
+                    <p>Share your flat with your buddies. Send them your code now.</p>
+                    <form role="form" action="<?= URL ?>/app/shareFlat" method="post">
+                        <div class="form-group">
+                            <input type="email" id="email" name="email" class="form-control" placeholder="Email Address" tabindex="1">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" value="Send Code" class="btn btn-primary" tabindex="2">
+                        </div>
+                    </form>
+                </div><!-- end panel-body -->
+            </div><!-- end shareFlat -->
+            <!-- Create Flat -->
             <div class="panel panel-primary" <?php echo (Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
                 <div class="panel-heading">
                     <h3 class="panel-title">Create Flat</h3>
@@ -30,7 +66,8 @@
                     </form>
                 </div><!-- end panel-body -->
             </div><!-- end createFlat -->
-            
+
+            <!-- Join Flat -->
             <div class="panel panel-primary" <?php echo (Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
                 <div class="panel-heading">
                     <h3 class="panel-title">Join Flat</h3>
@@ -54,6 +91,7 @@
                 </div><!-- end panel-body -->
             </div><!-- end joinFlat -->
 
+            <!-- Leave Flat -->
             <div class="panel panel-primary" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
                 <div class="panel-heading">
                     <h3 class="panel-title">Leave Flat</h3>
