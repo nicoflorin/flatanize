fd6b8efcf7ffb3fed0438f87e3667f93ca5d2310
Nico <nico_florin@gmx.ch>
2015-07-09 10:53:44 +0200

    create / getTaskList function hinzu.

--- app/models/cleaningModel.php
+++ app/models/cleaningModel.php
@@ -10,7 +10,50 @@ class cleaningModel extends Model{
     /**
      * Erstellt einen neuen Cleaning Task
      */
-    public function create($title, $freq, $wday, $start) {
+    public function create($flatId, $title, $freq, $wday, $start) {
+        //Hole wochenTag ID aus DB
+        $bind = array(':wday' => $wday);
+        $res = $this->db->select('id', 'wdays', 'day = :wday LIMIT 1', $bind);
+        $wdayId = $res[0]['id'];
         
+        //Hole Frequenz ID aus DB
+        $bind = array(':freq' => $freq);
+        $res = $this->db->select('id', 'frequencies', 'description = :freq LIMIT 1', $bind);
+        $freqId = $res[0]['id'];
+        
+        //Füge Daten in DB ein
+        $bind = array(
+            ':flatId' => $flatId,
+            ':freqId' => $freqId,
+            ':wdayId' => $wdayId,
+            ':title' => $title,
+            ':start' => $start
+        );
+        $res = $this->db->insert('cleanings', 'flats_id, frequencies_id, wdays_id, title, start', ':flatId, :freqId, :wdayId, :title, :start', $bind);
+        
+        //Wenn OK
+        if ($res == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * Holt anhand der flatId alle WG Tasks aus DB
+     * @param type $flatId
+     * @return boolean
+     */
+    public function getTaskList($flatId) {
+        //Inner Join auf cleanings, frequencies, wdays
+        $bind = array(':flatId' => $flatId);
+        $res = $this->db->select('a.id, a.flats_id, a.title, a.start, b.description, c.day',
+                'cleanings a, frequencies b, wdays c', 
+                'a.frequencies_id = b.id AND a.wdays_id = c.id AND flats_id = :flatId', $bind);
+        if (!empty($res)) {
+            return $res;
+        } else {
+            return false;
+        }
     }
 }
