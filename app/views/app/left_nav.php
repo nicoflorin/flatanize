7731910eb039d4e62612160b6894b9ff71a67042
Nico <nico_florin@gmx.ch>
2015-07-09 12:47:37 +0200

    create new task entfernt

--- app/views/app/left_nav.php
+++ app/views/app/left_nav.php
@@ -3,7 +3,7 @@
     <!-- @Todo Dynamisch active classe -->
         <li class="<?php echo ($this->data['title'] == 'Home') ? 'active' : '' ?>"><a href="<?= URL ?>">Home</a></li>
         <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/shopping/index">Shopping List</a></li>
-        <li class="<?php echo ($this->data['title'] == 'Cleaning Tasks' || $this->data['title'] == 'Create Task') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/cleaning/index">Cleaning Tasks</a></li>
+        <li class="<?php echo ($this->data['title'] == 'Cleaning Tasks') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/cleaning/index">Cleaning Tasks</a></li>
         <li class="<?php echo ($this->data['title'] == 'Financials') ? 'active' : '' ?>"><a href="#" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>Finances</a></li>
         <li class="nav-divider"></li>
         <li class="<?php echo ($this->data['title'] == 'Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/index">Settings</a></li>
