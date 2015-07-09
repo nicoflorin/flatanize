1fe0fd920f597aa15ffd4609564825574c966191
Nico <nico_florin@gmx.ch>
2015-07-09 10:54:55 +0200

    tab nav erstellt

--- app/views/settings/index.php
+++ app/views/settings/index.php
@@ -9,111 +9,127 @@
                 <h1><?= $this->data['title'] ?></h1>
             </div>
 
-            <!-- Flat Infos -->
-            <div class="panel panel-primary" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
-                <div class="panel-heading">
-                    <h3 class="panel-title"><?= $this->flatName ?></h3>
+            <!-- Tab Navigation -->
+            <ul class="nav nav-tabs">
+                <li class="active"><a data-toggle="tab" href="#flatSettings">Flat settings</a></li>
+                <li><a data-toggle="tab" href="#userSettings">User settings</a></li>
+            </ul><!-- end nav -->
+            <br />
+            <div class="tab-content">
+                <!-- Flat Settings tab -->
+                <div id="flatSettings" class="tab-pane fade in active">
+                    <!-- Flat Infos -->
+                    <div class="panel panel-primary" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
+                        <div class="panel-heading">
+                            <h3 class="panel-title"><?= $this->flatName ?> - Informations</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p>Your flat code is: <strong><?= $this->flatCode ?></strong></p>
+                            <p>You are in a flat with:</p>
+                            <p>@Todo Liste von allen mitglieder</p>
+                        </div><!-- end panel-body -->
+                    </div><!-- end flat infos -->
+
+                    <!-- Share Flat -->
+                    <div class="panel panel-primary" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Share your flat</h3>
+                        </div>
+                        <div class="panel-body">
+                            <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
+                            <?php
+                            if (isset($this->data['error_share'])) {
+                                echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
+                            }
+                            ?>
+                            <p>Share this flat with your buddies. Send them your code now.</p>
+                            <form role="form" action="<?= URL ?>/settings/shareFlat" method="post">
+                                <div class="form-group">
+                                    <input type="email" id="email" name="email" class="form-control" placeholder="Email Address" tabindex="1" required> 
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="submit" value="Send Code" class="btn btn-success" tabindex="2">
+                                </div>
+                            </form>
+                        </div><!-- end panel-body -->
+                    </div><!-- end shareFlat -->
+                    <!-- Create Flat -->
+                    <div class="panel panel-primary" <?php echo (Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Create flat</h3>
+                        </div>
+                        <div class="panel-body">
+                            <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
+                            <?php
+                            if (isset($this->data['error_create'])) {
+                                echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
+                            }
+                            ?>
+                            <form role="form" action="<?= URL ?>/settings/createFlat" method="post">
+                                <div class="form-group">
+                                    <input type="text" value="" id="flatName" name="flatName" class="form-control" placeholder="Flat Name" tabindex="1" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="submit" value="Create" class="btn btn-success" tabindex="2">
+                                </div>
+                            </form>
+                        </div><!-- end panel-body -->
+                    </div><!-- end createFlat -->
+
+                    <!-- Join Flat -->
+                    <div class="panel panel-primary" <?php echo (Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Join flat</h3>
+                        </div>
+                        <div class="panel-body">
+                            <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
+                            <?php
+                            if (isset($this->data['error_join'])) {
+                                echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
+                            }
+                            ?>
+                            <form role="form" action="<?= URL ?>/settings/joinFlat" method="post">
+                                <div class="form-group">
+                                    <input type="text" value="" id="flatCode" name="flatCode" class="form-control" placeholder="Flat Code" tabindex="1" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="submit" value="Join" class="btn btn-success" tabindex="2">
+                                </div>
+                            </form>
+                        </div><!-- end panel-body -->
+                    </div><!-- end joinFlat -->
+
+                    <!-- Leave Flat -->
+                    <div class="panel panel-primary" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Leave your flat</h3>
+                        </div>
+                        <div class="panel-body">
+                            <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
+                            <?php
+                            if (isset($this->data['error_leave'])) {
+                                echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
+                            }
+                            ?>
+                            <p>You really want to leave your flat?</p>
+                            <form role="form" action="<?= URL ?>/settings/leaveFlat" method="post">
+                                <div class="form-group">
+                                    <input type="submit" value="Yes, leave" class="btn btn-success">
+                                </div>
+                            </form>
+                        </div><!-- end panel-body -->
+                    </div><!-- end leaveFlat -->
                 </div>
-                <div class="panel-body">
-                    <p>Your flat code is: <?= $this->flatCode ?></p>
-                    <p>You are in a flat with:</p>
-                    <p>@Todo Liste von allen mitglieder</p>
-                </div><!-- end panel-body -->
-            </div><!-- end flat infos -->
-
-            <!-- Share Flat -->
-            <div class="panel panel-primary" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
-                <div class="panel-heading">
-                    <h3 class="panel-title">Share your flat</h3>
+                <!-- User Settings tab -->
+                <div id="userSettings" class="tab-pane fade">
+                    <!-- @Todo Passwort ändern -->
+                    <!-- @Todo Bild hochladen -->
+                    <!-- @Todo Anzeigenamen ändern -->
                 </div>
-                <div class="panel-body">
-                    <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
-                    <?php
-                    if (isset($this->data['error_share'])) {
-                        echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
-                    }
-                    ?>
-                    <p>Share your flat with your buddies. Send them your code now.</p>
-                    <form role="form" action="<?= URL ?>/settings/shareFlat" method="post">
-                        <div class="form-group">
-                            <input type="email" id="email" name="email" class="form-control" placeholder="Email Address" tabindex="1" required> 
-                        </div>
-
-                        <div class="form-group">
-                            <input type="submit" value="Send Code" class="btn btn-success" tabindex="2">
-                        </div>
-                    </form>
-                </div><!-- end panel-body -->
-            </div><!-- end shareFlat -->
-            <!-- Create Flat -->
-            <div class="panel panel-primary" <?php echo (Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
-                <div class="panel-heading">
-                    <h3 class="panel-title">Create flat</h3>
-                </div>
-                <div class="panel-body">
-                    <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
-                    <?php
-                    if (isset($this->data['error_create'])) {
-                        echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
-                    }
-                    ?>
-                    <form role="form" action="<?= URL ?>/settings/createFlat" method="post">
-                        <div class="form-group">
-                            <input type="text" value="" id="flatName" name="flatName" class="form-control" placeholder="Flat Name" tabindex="1" required>
-                        </div>
-
-                        <div class="form-group">
-                            <input type="submit" value="Create" class="btn btn-success" tabindex="2">
-                        </div>
-                    </form>
-                </div><!-- end panel-body -->
-            </div><!-- end createFlat -->
-
-            <!-- Join Flat -->
-            <div class="panel panel-primary" <?php echo (Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
-                <div class="panel-heading">
-                    <h3 class="panel-title">Join flat</h3>
-                </div>
-                <div class="panel-body">
-                    <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
-                    <?php
-                    if (isset($this->data['error_join'])) {
-                        echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
-                    }
-                    ?>
-                    <form role="form" action="<?= URL ?>/settings/joinFlat" method="post">
-                        <div class="form-group">
-                            <input type="text" value="" id="flatCode" name="flatCode" class="form-control" placeholder="Flat Code" tabindex="1" required>
-                        </div>
-
-                        <div class="form-group">
-                            <input type="submit" value="Join" class="btn btn-success" tabindex="2">
-                        </div>
-                    </form>
-                </div><!-- end panel-body -->
-            </div><!-- end joinFlat -->
-
-            <!-- Leave Flat -->
-            <div class="panel panel-primary" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>
-                <div class="panel-heading">
-                    <h3 class="panel-title">Leave your flat</h3>
-                </div>
-                <div class="panel-body">
-                    <!-- Error Fenster anzeigen, falls ein Fehler auftrat -->
-                    <?php
-                    if (isset($this->data['error_leave'])) {
-                        echo '<div class="alert alert-danger" role="alert"><p>' . $this->data['error_msg'] . '</p></div>';
-                    }
-                    ?>
-                    <p>You really want to leave your flat?</p>
-                    <form role="form" action="<?= URL ?>/settings/leaveFlat" method="post">
-                        <div class="form-group">
-                            <input type="submit" value="Yes, leave" class="btn btn-success">
-                        </div>
-                    </form>
-                </div><!-- end panel-body -->
-            </div><!-- end leaveFlat -->
-
+            </div><!-- end tab-content -->
         </div><!-- end well -->
     </div><!-- end col -->
 </div><!-- end row -->
