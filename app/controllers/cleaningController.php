a7951165627b674a15a3099df12edc2e301ac5d3
Nico <nico_florin@gmx.ch>
2015-07-09 10:55:47 +0200

    validateDate / createTask

--- app/controllers/cleaningController.php
+++ app/controllers/cleaningController.php
@@ -16,25 +16,79 @@ class CleaningController extends Controller {
      * Lädt Cleaning index Seite
      */
     public function index() {
+        $this->loadModel('cleaning');
+        $flatId = Session::getFlatId();
+        //Hole Liste von Tasks
+        $this->view->taskList = $this->model->getTaskList($flatId);
         $this->view->render('cleaning/index', 'Cleaning Tasks');
     }
+    
 
     /**
      * Lädt Seite um neuen Task zu erstellen
      */
-    public function showCreateTask() {
+    public function showCreateTask($error = '') {
+        // error == 1 bedeutet datum im falschen Format
+        if ($error == 1) {
+            $this->view->assign('date', true);
+        }
         $this->view->render('cleaning/create_task', 'Create Task');
     }
-    
+
     public function createTask() {
         $title = $_POST['title'];
         $freq = $_POST['frequency'];
         $wday = $_POST['weekday'];
         $start = $_POST['start'];
-        
-        
-        $this->loadModel('cleaning');
-        $this->model->create($title, $freq, $wday, $start);
+        $flatId = Session::getFlatId();
+
+        $error = [];
+        //Prüfe ob Datum format 
+        if ($this->validateDate($start)) { //Y-m-d
+
+        } else if ($this->validateDate($start, 'd.m.Y')) {
+            $start = $this->formatDate($start); //formatiere Datum in Format y-m-d
+        } else { //Kein Datum im gewünschten Format
+            $error['date'] = true;
+        }
+
+        //Wenn keine Fehler auftraten
+        if (empty($error)) {
+            $this->loadModel('cleaning');
+            $res = $this->model->create($flatId, $title, $freq, $wday, $start);
+            if ($res === true) {
+                $this->redirect('cleaning', 'index');
+            }else {
+                $this->redirect('cleaning', 'showCreateTask');
+            }
+        } else {
+            $this->redirect('cleaning', 'showCreateTask', $error);
+        }
+    }
+
+    /**
+     * Prüft einen String auf das korrekte Datum Format
+     * @param string $date
+     * @param string $format
+     * @return boolean
+     */
+    public function validateDate($date, $format = 'Y-m-d') {
+        try {
+            $d = DateTime::createFromFormat($format, $date);
+            return $d && $d->format($format) == $date;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Formatiert ein DateTime in das gewünschte Format
+     * @param DateTime $date
+     * @param string $format
+     */
+    public function formatDate($date, $format = 'Y-m-d') {
+        $out = new DateTime($date);
+        return $out->format($format);
     }
 
 }
