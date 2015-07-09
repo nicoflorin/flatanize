de45819f8547228a372451328fc33d1359f9d24a
Nico <nico_florin@gmx.ch>
2015-07-09 10:53:21 +0200

    Create Task hinzugefügt

--- app/views/app/left_nav.php
+++ app/views/app/left_nav.php
@@ -1,8 +1,9 @@
 <div class="well">
 <ul class="nav nav-pills nav-stacked">
+    <!-- @Todo Dynamisch active classe -->
         <li class="<?php echo ($this->data['title'] == 'Home') ? 'active' : '' ?>"><a href="<?= URL ?>">Home</a></li>
         <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>"><a href="<?= URL ?>/shopping/index">Shopping List</a></li>
-        <li class="<?php echo ($this->data['title'] == 'Cleaning Tasks') ? 'active' : '' ?>"><a href="<?= URL ?>/cleaning/index">Cleaning Tasks</a></li>
+        <li class="<?php echo ($this->data['title'] == 'Cleaning Tasks' || $this->data['title'] == 'Create Task') ? 'active' : '' ?>"><a href="<?= URL ?>/cleaning/index">Cleaning Tasks</a></li>
         <li class="<?php echo ($this->data['title'] == 'Financials') ? 'active' : '' ?>"><a href="#">Finances</a></li>
         <li class="nav-divider"></li>
         <li class="<?php echo ($this->data['title'] == 'Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/index">Settings</a></li>
