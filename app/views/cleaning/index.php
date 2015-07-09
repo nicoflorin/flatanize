d8ba99937d83454c7033d0df1711707485e8d03a
Nico <nico_florin@gmx.ch>
2015-07-09 10:54:14 +0200

    br hinz.

--- app/views/cleaning/index.php
+++ app/views/cleaning/index.php
@@ -13,11 +13,35 @@
             <div>
                 <a class="btn btn-success" href="<?= URL ?>/cleaning/showCreateTask" role="button">Create New Task</a>
             </div>
-            
+            <br>
             <!-- Flat schedules -->
-            <div>
-                <h3>Flat schedules</h3>
-            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Scheduled Tasks</h3>
+                </div>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th class="col-xs-7">Title</th>
+                            <th class="col-xs-2">Freq.</th>
+                            <th class="col-xs-1">On</th>
+                            <th class="col-xs-2">Start</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($this->taskList as $value) {
+                            echo '<tr>';
+                            echo '<td>' . $value['title'] . '</td>';
+                            echo '<td>' . $value['description'] . '</td>';
+                            echo '<td>' . $value['day'] . '</td>';
+                            echo '<td>' . $value['start'] . '</td>';
+                            echo '</tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div><!-- end panel -->
         </div><!-- end well -->
     </div><!-- end col -->
 </div><!-- end row -->
