be833bf4a1a583c7c6bb097b187ca7b48fe889b9
Nico <nico_florin@gmx.ch>
2015-07-10 19:08:02 +0200

    Weekday Const hinzu.

--- app/configs/consts.php
+++ app/configs/consts.php
@@ -3,5 +3,12 @@ define('ONCE', 'once');
 define('DAILY', 'daily');
 define('WEEKLY', 'weekly');
 define('MONTHLY', 'every month');
+define('MONDAY', 'Monday');
+define('TUESDAY', 'Tuesday');
+define('WEDNESDAY', 'Wednesday');
+define('THURSDAY', 'Thursday');
+define('FRIDAY', 'Friday');
+define('SATURDAY', 'Saturday');
+define('SUNDAY', 'Sunday');
 define('URL', 'http://'.$_SERVER['HTTP_HOST'].'/flatanize');
 // ROOT in public/index.php definiert. ROOT => flatanize
