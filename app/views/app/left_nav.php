cacc5fd2ad3a55279493ecccb8931be44f3cd32b
Nico <nico_florin@gmx.ch>
2015-07-10 14:58:32 +0200

    mobile view

--- app/views/app/left_nav.php
+++ app/views/app/left_nav.php
@@ -1,17 +1,19 @@
-<div class="navbar-header">
-    <!-- button for menu when on mobile -->
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#leftNavCollapse">
-        <b class="caret"></b>
-    </button>
-</div><!-- end navbar-header -->
-<div class="well collapse" id="leftNavCollapse">
-    <ul class="nav nav-pills nav-stacked">
-        <!-- @Todo Dynamisch active classe -->
-        <li class="<?php echo ($this->data['title'] == 'Home') ? 'active' : '' ?>"><a href="<?= URL ?>">Home</a></li>
-        <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/shopping/index">Shopping List</a></li>
-        <li class="<?php echo ($this->data['title'] == 'Cleaning Tasks') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/cleaning/index">Cleaning Tasks</a></li>
-        <li class="<?php echo ($this->data['title'] == 'Financials') ? 'active' : '' ?>"><a href="#" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>Finances</a></li>
-        <li class="nav-divider"></li>
-        <li class="<?php echo ($this->data['title'] == 'Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/index">Settings</a></li>
-    </ul>
+<div>
+    <div class="navbar-header">
+        <!-- button für menü auf mobile -->
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#leftNavCollapse">
+            <b class="caret"></b>
+        </button>
+    </div><!-- end navbar-header -->
+    <div class="collapse navbar-collapse" id="leftNavCollapse">
+        <ul class="nav nav-pills nav-stacked">
+            <!-- @Todo Dynamisch active classe -->
+            <li class="<?php echo ($this->data['title'] == 'Home') ? 'active' : '' ?>"><a href="<?= URL ?>">Home</a></li>
+            <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/shopping/index">Shopping List</a></li>
+            <li class="<?php echo ($this->data['title'] == 'Cleaning Tasks') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/cleaning/index">Cleaning Tasks</a></li>
+            <li class="<?php echo ($this->data['title'] == 'Financials') ? 'active' : '' ?>"><a href="#" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>>Finances</a></li>
+            <li class="nav-divider"></li>
+            <li class="<?php echo ($this->data['title'] == 'Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/index">Settings</a></li>
+        </ul>
+    </div>
 </div>
