ec60ec0f8c02528224d9c42d7e2e8c111c3dd006
Nico <nico_florin@gmx.ch>
2015-07-10 18:33:50 +0200

    paths gelöscht / consts erstellt

--- app/configs/consts.php
+++ app/configs/consts.php
@@ -1,8 +1,7 @@
 <?php
-
 define('ONCE', 'once');
 define('DAILY', 'daily');
 define('WEEKLY', 'weekly');
 define('MONTHLY', 'every month');
-
-
+define('URL', 'http://'.$_SERVER['HTTP_HOST'].'/flatanize');
+// ROOT in public/index.php definiert. ROOT => flatanize
