185d78cebdce33b4bcdb8a90cfb9b44cee93375d
Nico <nico_florin@gmx.ch>
2015-07-11 12:48:01 +0200

    seite aufgeteilt

---
+++ app/views/settings/userSettings.php
@@ -0,0 +1,6 @@
+<!-- User Settings tab -->
+<div id="userSettings" class="tab-pane fade">
+    <!-- @Todo Passwort ändern -->
+    <!-- @Todo Bild hochladen -->
+    <!-- @Todo Anzeigenamen ändern -->
+</div>
