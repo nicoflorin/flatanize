6195a30b9fbc8d69f7ac98c0af7f835dcd4da821
Nico <nico_florin@gmx.ch>
2015-07-11 14:22:21 +0200

    Financials zu Finances umbenannt

--- app/views/app/left_nav.php
+++ app/views/app/left_nav.php
@@ -11,7 +11,7 @@
             <li class="<?php echo ($this->data['title'] == 'Home') ? 'active' : '' ?>"><a href="<?= URL ?>">Home</a></li>
             <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/shopping/index">Shopping List</a></li>
             <li class="<?php echo ($this->data['title'] == 'Task Scheduling') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/task/index">Task Scheduling</a></li>
-            <li class="<?php echo ($this->data['title'] == 'Financials') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/finance/index" >Finances</a></li>
+            <li class="<?php echo ($this->data['title'] == 'Finances') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/finance/index" >Finances</a></li>
             <li class="nav-divider"></li>
             <li class="<?php echo ($this->data['title'] == 'Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/index">Settings</a></li>
         </ul>
