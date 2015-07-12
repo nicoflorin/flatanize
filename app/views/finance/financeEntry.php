c7643376151b7d09eb9069caafe6814164e9deb0
Nico <nico_florin@gmx.ch>
2015-07-12 18:50:07 +0200

    Darstellung entries / modal initial

--- app/views/finance/financeEntry.php
+++ app/views/finance/financeEntry.php
@@ -16,9 +16,10 @@
         <table class="table">
             <thead class="nopadding">
                 <tr>
-                    <th class="col-xs-5 nopadding"></th>
-                    <th class="col-xs-3 nopadding"></th>
                     <th class="col-xs-4 nopadding"></th>
+                    <th class="col-xs-2 nopadding"></th>
+                    <th class="col-xs-5 nopadding"></th>
+                    <th class="col-xs-1 nopadding"></th>
                 </tr>
             </thead>
             <tbody>
@@ -31,19 +32,42 @@
                     //Datum in Format d.m.Y formatieren
                     $entry['date'] = Functions::formatDate($entry['date'], 'd.m.Y');
                     ?>
+
                     <tr>
                         <td><strong><?= $entry['product'] ?></strong>
                             <p><?= $entry['date'] ?></p>
                         </td>
-                        <td>Added by: <?= $entry['display_name'] ?></td>
-                        <td class="text-right"><?= $entry['price'] ?> <?= CURR ?>
-                            <p><?= $entry['pricePP'] ?>  <?= CURR ?></p>
+                        <td><?= $entry['display_name'] ?></td>
+                        <td class="text-right"><?= number_format($entry['price'], 2, '.', '') ?> <?= CURR ?>
+                            <p><?= number_format($entry['pricePP'], 2, '.', '') ?>  <?= CURR ?></p>
                         </td>
-                    </tr>   
+                        <td><a href="#financeInfo" class="btn btn-sm btn-primary" data-toggle="modal"><span class="glyphicon glyphicon-info-sign"></span></a></td>
+                    </tr>
                     <?php
                 }
                 ?>
         </table>
+
+        <div>
+            <!-- Modal für Finance Information -->
+            <div id="financeInfo" class="modal fade">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title">Information</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p>Do you want to save changes you made to document before closing?</p>
+                            <p class="text-warning"><small>If you don't save, your changes will be lost.</small></p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>     
 
     </div><!-- end panel -->
 </div>
