67a66c1921282dc4f230f41763beefe4cf58d5da
Nico <nico_florin@gmx.ch>
2015-07-14 11:29:30 +0200

    Settings Link angepasst

--- app/views/nav_sec.php
+++ app/views/nav_sec.php
@@ -27,7 +27,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Your Account <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="<?= URL ?>/app/settings"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
+                        <li><a href="<?= URL ?>/settings/index"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
                         <li class="divider"></li>
                         <li><a href="<?= URL ?>/auth/logout"><span class="glyphicon glyphicon-off"></span> Log Out</a></li>
                     </ul>
