3b1a46f40c83777de21f3f1d687c7c5818ed83b4
Nico <nico_florin@gmx.ch>
2015-07-14 11:36:21 +0200

    leaveFlat $res entfernt

--- app/controllers/settingsController.php
+++ app/controllers/settingsController.php
@@ -27,7 +27,6 @@ class SettingsController extends Controller {
             //Hole Usernames und übergebe an view
             $this->loadModel('user');
             $this->view->users = $this->model->getAllDisplayNames($flatId);
-            ;
         }
         $this->view->render('settings/index', 'Settings');
     }
@@ -56,7 +55,7 @@ class SettingsController extends Controller {
     public function leaveFlat() {
         $userId = Session::get('user_id');
         $this->loadModel('flat');
-        $res = $this->model->leave($userId);
+        $this->model->leave($userId);
 
         $this->redirect('settings', 'index');
     }
