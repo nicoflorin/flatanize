9072fe5ef1061bb05c836a5b0236080a28363d9d
Nico <nico_florin@gmx.ch>
2015-07-15 12:28:30 +0200

    Eigene Einträge für flatSettings und userSettings

--- app/views/app/left_nav.php
+++ app/views/app/left_nav.php
@@ -7,13 +7,15 @@
     </div><!-- end navbar-header -->
     <div class="collapse navbar-collapse" id="leftNavCollapse">
         <ul class="nav nav-pills nav-stacked">
-            <!-- @Todo Dynamisch active classe -->
+            <!-- @Todo Dynamisch active class -->
             <li class="<?php echo ($this->data['title'] == 'Home') ? 'active' : '' ?>"><a href="<?= URL ?>">Home</a></li>
             <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/shopping/index">Shopping List</a></li>
             <li class="<?php echo ($this->data['title'] == 'Task Scheduling') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/task/index">Task Scheduling</a></li>
             <li class="<?php echo ($this->data['title'] == 'Finances') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/finance/index" >Finances</a></li>
             <li class="nav-divider"></li>
-            <li class="<?php echo ($this->data['title'] == 'Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/index">Settings</a></li>
+            <li id="NavSettings"><a>Settings</a></li>
+            <li class="<?php echo ($this->data['title'] == 'Flat Settings') ? 'active' : '' ?> indent"><a href="<?= URL ?>/settings/flatSettings">Flat Settings</a></li>
+            <li class="<?php echo ($this->data['title'] == 'User Settings') ? 'active' : '' ?> indent"><a href="<?= URL ?>/settings/userSettings">User Settings</a></li>
         </ul>
     </div>
 </div>
