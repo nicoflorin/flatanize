023635e9c2dd1c5ecbc68b2759a1347920dff49e
Nico <nico_florin@gmx.ch>
2015-07-15 16:57:11 +0200

    Autoloader erstellt

--- public/index.php
+++ public/index.php
@@ -3,19 +3,30 @@
  * Haupt-Einstiegspunkt.
  * Alle Zugriffe werden über diese Datei geroutet.
  */
-define('ROOT', realpath(dirname(__DIR__))); // realpath gibt eindeutigen und absoluten Pfadnamen zurück
 
-//Library
-require_once (ROOT . '/app/libs/App.php');
-require_once (ROOT . '/app/libs/Controller.php');
-require_once (ROOT . '/app/libs/Database.php');
-require_once (ROOT . '/app/libs/Model.php');
-require_once (ROOT . '/app/libs/Session.php');
-require_once (ROOT . '/app/libs/View.php');
-require_once (ROOT . '/app/libs/Functions.php');
+//Konstante für ROOT Verzeichnis (Filestruktur)
+define('ROOT', realpath(dirname(__DIR__))); // realpath gibt eindeutigen und absoluten Pfadnamen zurück
 
 //Configs
 require_once (ROOT . '/app/configs/consts.php');
+
+/**
+ * Autoloader
+ * Lädt automatisch alle benötigten Klassen
+ * @param string $class
+ */
+function __autoload($class) {
+    require_once ROOT . '/app/libs/' . $class . '.php';
+}
+//Library
+//@Todo entfernen
+//require_once (ROOT . '/app/libs/App.php');
+//require_once (ROOT . '/app/libs/Controller.php');
+//require_once (ROOT . '/app/libs/Database.php');
+//require_once (ROOT . '/app/libs/Model.php');
+//require_once (ROOT . '/app/libs/Session.php');
+//require_once (ROOT . '/app/libs/View.php');
+//require_once (ROOT . '/app/libs/Functions.php');
 
 // Starte Hauptscript
 $app = new App();
