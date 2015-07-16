78b789a1b237491e37c00586cb7d7783e7c84825
Nico <nico_florin@gmx.ch>
2015-07-16 15:30:41 +0200

    id="rightContent" class="col-sm-9

--- app/views/app/index.php
+++ app/views/app/index.php
@@ -1,9 +1,9 @@
 <div class="row">
-    <div class="col-md-3 col-xs-12">
-        <?php require_once ROOT . '/app/views/app/' . 'left_nav.php'; ?>
+    <div class="col-sm-3 col-xs-12">
+        <?php require_once ROOT . '/app/views/app/left_nav.php'; ?>
     </div>
 
-    <div class="col-md-9 col-xs-12">
+    <div id="rightContent" class="col-sm-9 col-xs-12">
         <div class="well">
             <div class="page-header">
                 <h1>Hi <?= $this->displayName ?>!</h1>
