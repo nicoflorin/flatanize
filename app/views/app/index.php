f8c599366a5cc2ca48ad2c543383edc57f9928a3
Nico <nico_florin@gmx.ch>
2015-07-17 15:52:30 +0200

    gelöscht

--- app/views/app/index.php
+++
@@ -1,8 +0,0 @@
-<?php require_once ROOT . '/app/views/app/left_nav.php'; ?>
-<div id="mainContent" class="well">
-    <!-- Label damit ganzer Content Rechts clickbar wird, wenn collapsed -->
-    <label id="bigLabel" for="leftMenuBtn" class="bigLabel"></label>
-    <div class="page-header">
-        <h1>Hi <?= $this->displayName ?>!</h1>
-    </div>
-</div><!-- end mainContent -->
