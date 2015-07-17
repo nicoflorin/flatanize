b25da035941f9dcb65b1f004039118ae59e480ec
Nico <nico_florin@gmx.ch>
2015-07-17 11:00:32 +0200

    Setze Cookie falls remembered true

--- app/models/loginModel.php
+++ app/models/loginModel.php
@@ -14,20 +14,36 @@ class LoginModel extends Model {
     /**
      * Verarbeitet Login
      */
-    public function login($username, $password) {
+    public function login($username, $password, $remember) {
         // Select auf diesen Usernamen
         $bind = array(':username' => $username);
         $res = $this->db->select('id, flats_id, password, salt', 'users', 'username = :username LIMIT 1', $bind);
 
         // Bei Treffer auf DB
-        if (!empty($res)) { 
+        if (!empty($res)) {
             $passwordFromDB = $res[0]['password'];
             $saltFromDB = $res[0]['salt'];
             $flatId = $res[0]['flats_id'];
             $userId = $res[0]['id'];
 
             // Vergleiche Input Password mit Hash, Salt aus DB
-            if (Functions::testPassword($password, $saltFromDB, $passwordFromDB)) { // Wenn Password übereinstimmt
+            if (Functions::testHash($password, $saltFromDB, $passwordFromDB)) { // Wenn Password übereinstimmt
+                //Falls remember me gesetzt, token in db und als cookie speichern
+                if ($remember === true) {
+                    $random = Functions::generateRandomData(64);
+                    $token = Functions::hash($random);
+
+                    //Speichere Token in DB
+                    $bind = array(
+                        ':userId' => $userId,
+                        ':token' => $token
+                    );
+                    $res = $this->db->update('users', 'token = :token', 'id = :userId', $bind);
+                    $expire = 3600 * 24 * 365; //1 year
+                    //Setze Cookie
+                    Cookie::set('token', $token, $expire);
+                }
+
                 //Schreibe User Session var
                 Session::setUserId($userId);
 
@@ -35,7 +51,7 @@ class LoginModel extends Model {
                 if (isset($flatId)) {
                     Session::setFlatId($flatId);
                 }
-                
+
                 return true;
             } else { //wenn Password falsch
                 return false;
