93d598433348bdb8257f3d5e99bfffc5a1ffc460
Nico <nico_florin@gmx.ch>
2015-07-19 22:33:50 +0200

    include stayInWebApp.js

--- app/views/footer.php
+++ app/views/footer.php
@@ -14,8 +14,10 @@
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <!-- Bootstrap's JavaScript plugin -->
-        <script src="<?php echo URL ?>/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?php echo URL ?>/js/bootstrap.min.js"></script>
         <!-- toggle left menu script -->
-        <script src="<?php echo URL ?>/js/toggle_left_nav.js"></script>
+        <script type="text/javascript" src="<?php echo URL ?>/js/toggle_left_nav.js"></script>
+        <!-- stay in web app script -->
+        <script type="text/javascript" src="<?php echo URL ?>/js/stayInWebApp.js"></script>
     </body>
 </html> 
