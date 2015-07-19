09f0d160a7065203f26b2ffb9e751ba5e26c80ee
Nico <nico_florin@gmx.ch>
2015-07-19 17:54:17 +0200

    maximum-scale=1 entfernt

--- app/views/header.php
+++ app/views/header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <!-- mobile zuerst, kein zoomen auf Smartphone möglich -->
-        <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <title><?= TITLE ?> - <?= $this->data['title'] ?></title>
         <!-- @Todo Meta tags wie description, keywords, author usw. -->
 
