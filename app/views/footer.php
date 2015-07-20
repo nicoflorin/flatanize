97581d80ca88363cb4b686a9ab2b5e308fffd250
Nico <nico_florin@gmx.ch>
2015-07-20 11:18:35 +0200

    footer hinz.

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,23 +1,21 @@
-        </div><!-- end main container -->
+</div><!-- end main container -->
 
-        </div> <!-- end wrapper -->
-        <!-- Footer only on large devices -->
-        <!--
-        <footer class="hidden-xs">
-            <div class="containter">
-                <div class="text-center text-muted">
-                    <h5>&copy; <?php echo date("Y") ?> - Nico Florin All Rights Reserved</h5> 
-                </div>
-            </div>
-        </footer>
-        -->
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <!-- Bootstrap's JavaScript plugin -->
-        <script type="text/javascript" src="<?php echo URL ?>/js/bootstrap.min.js"></script>
-        <!-- toggle left menu script -->
-        <script type="text/javascript" src="<?php echo URL ?>/js/toggle_left_nav.js"></script>
-        <!-- stay in web app script -->
-        <script type="text/javascript" src="<?php echo URL ?>/js/stayInWebApp.js"></script>
-    </body>
+</div> <!-- end wrapper -->
+<!-- Footer only on large devices -->
+
+<footer class="hidden-xs">
+    <div class="text-center text-muted">
+        <p>&copy; <?php echo date("Y") ?> - Nico Florin All Rights Reserved</p> 
+    </div>
+</footer>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Bootstrap's JavaScript plugin -->
+<script type="text/javascript" src="<?php echo URL ?>/js/bootstrap.min.js"></script>
+<!-- toggle left menu script -->
+<script type="text/javascript" src="<?php echo URL ?>/js/toggle_left_nav.js"></script>
+<!-- stay in web app script -->
+<!-- <script type="text/javascript" src="<?php echo URL ?>/js/stayInWebApp.js"></script> -->
+</body>
 </html> 
