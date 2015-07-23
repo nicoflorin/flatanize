09e9211c228dd13e6758a0bb6817af0d85a948bf
Nico <nico_florin@gmx.ch>
2015-07-23 14:01:40 +0200

    db name / user angepasst auf flatanize_com

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,24 +1,17 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- -----------------------------------------------------
+-- Schema flatanize_com
+-- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema flatanize
+-- Schema flatanize_com
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `flatanize` ;
+CREATE SCHEMA IF NOT EXISTS `flatanize_com` DEFAULT CHARACTER SET latin1 ;
+USE `flatanize_com` ;
 
 -- -----------------------------------------------------
--- Schema flatanize
+-- Table `flatanize_com`.`flats`
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `flatanize` DEFAULT CHARACTER SET latin1 ;
-USE `flatanize` ;
-
--- -----------------------------------------------------
--- Table `flatanize`.`flats`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flatanize`.`flats` (
+CREATE TABLE IF NOT EXISTS `flatanize_com`.`flats` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `code` CHAR(5) NOT NULL,
@@ -28,9 +21,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `flatanize`.`users`
+-- Table `flatanize_com`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flatanize`.`users` (
+CREATE TABLE IF NOT EXISTS `flatanize_com`.`users` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `flats_id` INT UNSIGNED NULL,
   `username` VARCHAR(50) NOT NULL,
@@ -44,16 +37,16 @@ CREATE TABLE IF NOT EXISTS `flatanize`.`users` (
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_users_flats`
     FOREIGN KEY (`flats_id`)
-    REFERENCES `flatanize`.`flats` (`id`)
+    REFERENCES `flatanize_com`.`flats` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `flatanize`.`shopping_lists`
+-- Table `flatanize_com`.`shopping_lists`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flatanize`.`shopping_lists` (
+CREATE TABLE IF NOT EXISTS `flatanize_com`.`shopping_lists` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `flats_id` INT UNSIGNED NOT NULL,
   `added_by` INT UNSIGNED NOT NULL,
@@ -64,21 +57,21 @@ CREATE TABLE IF NOT EXISTS `flatanize`.`shopping_lists` (
   INDEX `fk_shopping_users_idx` (`added_by` ASC),
   CONSTRAINT `fk_shopping_lists_flats`
     FOREIGN KEY (`flats_id`)
-    REFERENCES `flatanize`.`flats` (`id`)
+    REFERENCES `flatanize_com`.`flats` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_shopping_lists_users`
     FOREIGN KEY (`added_by`)
-    REFERENCES `flatanize`.`users` (`id`)
+    REFERENCES `flatanize_com`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `flatanize`.`finances`
+-- Table `flatanize_com`.`finances`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flatanize`.`finances` (
+CREATE TABLE IF NOT EXISTS `flatanize_com`.`finances` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `flats_id` INT UNSIGNED NOT NULL,
   `added_by` INT UNSIGNED NOT NULL,
@@ -91,21 +84,21 @@ CREATE TABLE IF NOT EXISTS `flatanize`.`finances` (
   INDEX `fk_finances_users_idx` (`added_by` ASC),
   CONSTRAINT `fk_finances_flats`
     FOREIGN KEY (`flats_id`)
-    REFERENCES `flatanize`.`flats` (`id`)
+    REFERENCES `flatanize_com`.`flats` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_finances_users`
     FOREIGN KEY (`added_by`)
-    REFERENCES `flatanize`.`users` (`id`)
+    REFERENCES `flatanize_com`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `flatanize`.`frequencies`
+-- Table `flatanize_com`.`frequencies`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flatanize`.`frequencies` (
+CREATE TABLE IF NOT EXISTS `flatanize_com`.`frequencies` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
@@ -113,9 +106,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `flatanize`.`tasks`
+-- Table `flatanize_com`.`tasks`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flatanize`.`tasks` (
+CREATE TABLE IF NOT EXISTS `flatanize_com`.`tasks` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `flats_id` INT UNSIGNED NOT NULL,
   `frequencies_id` INT UNSIGNED NOT NULL,
@@ -126,66 +119,68 @@ CREATE TABLE IF NOT EXISTS `flatanize`.`tasks` (
   INDEX `fk_tasks_frequencies_idx` (`frequencies_id` ASC),
   CONSTRAINT `fk_tasks_flats`
     FOREIGN KEY (`flats_id`)
-    REFERENCES `flatanize`.`flats` (`id`)
+    REFERENCES `flatanize_com`.`flats` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_tasks_frequencies`
     FOREIGN KEY (`frequencies_id`)
-    REFERENCES `flatanize`.`frequencies` (`id`)
+    REFERENCES `flatanize_com`.`frequencies` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `flatanize`.`tasks_users`
+-- Table `flatanize_com`.`tasks_users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flatanize`.`tasks_users` (
+CREATE TABLE IF NOT EXISTS `flatanize_com`.`tasks_users` (
   `tasks_id` INT UNSIGNED NOT NULL,
   `users_id` INT UNSIGNED NOT NULL,
   `user_order` INT UNSIGNED NOT NULL,
   `count` INT UNSIGNED NOT NULL DEFAULT 0,
+  `timestamp` TIMESTAMP NOT NULL,
   PRIMARY KEY (`tasks_id`, `users_id`),
   INDEX `fk_tasks_users_us_idx` (`users_id` ASC),
   CONSTRAINT `fk_tasks_users_tasks`
     FOREIGN KEY (`tasks_id`)
-    REFERENCES `flatanize`.`tasks` (`id`)
+    REFERENCES `flatanize_com`.`tasks` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_tasks_users_users`
     FOREIGN KEY (`users_id`)
-    REFERENCES `flatanize`.`users` (`id`)
+    REFERENCES `flatanize_com`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `flatanize`.`finances_users`
+-- Table `flatanize_com`.`finances_users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flatanize`.`finances_users` (
+CREATE TABLE IF NOT EXISTS `flatanize_com`.`finances_users` (
   `finances_id` INT UNSIGNED NOT NULL,
   `users_id` INT UNSIGNED NOT NULL,
   `pricePP` FLOAT NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL,
   PRIMARY KEY (`finances_id`, `users_id`),
   INDEX `fk_fin_users_users_id_idx` (`users_id` ASC),
   CONSTRAINT `fk_fin_users_finances`
     FOREIGN KEY (`finances_id`)
-    REFERENCES `flatanize`.`finances` (`id`)
+    REFERENCES `flatanize_com`.`finances` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_fin_users_users`
     FOREIGN KEY (`users_id`)
-    REFERENCES `flatanize`.`users` (`id`)
+    REFERENCES `flatanize_com`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `flatanize`.`whiteboards`
+-- Table `flatanize_com`.`whiteboards`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flatanize`.`whiteboards` (
+CREATE TABLE IF NOT EXISTS `flatanize_com`.`whiteboards` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `users_id` INT UNSIGNED NOT NULL,
   `flats_id` INT UNSIGNED NOT NULL,
@@ -196,30 +191,25 @@ CREATE TABLE IF NOT EXISTS `flatanize`.`whiteboards` (
   INDEX `fk_whiteboard_flats_idx` (`flats_id` ASC),
   CONSTRAINT `fk_whiteboards_users`
     FOREIGN KEY (`users_id`)
-    REFERENCES `flatanize`.`users` (`id`)
+    REFERENCES `flatanize_com`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_whiteboards_flats`
     FOREIGN KEY (`flats_id`)
-    REFERENCES `flatanize`.`flats` (`id`)
+    REFERENCES `flatanize_com`.`flats` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
 -- -----------------------------------------------------
--- Data for table `flatanize`.`frequencies`
+-- Data for table `flatanize_com`.`frequencies`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `flatanize`;
-INSERT INTO `flatanize`.`frequencies` (`id`, `description`) VALUES (DEFAULT, 'once');
-INSERT INTO `flatanize`.`frequencies` (`id`, `description`) VALUES (DEFAULT, 'daily');
-INSERT INTO `flatanize`.`frequencies` (`id`, `description`) VALUES (DEFAULT, 'weekly');
-INSERT INTO `flatanize`.`frequencies` (`id`, `description`) VALUES (DEFAULT, 'every month');
+USE `flatanize_com`;
+INSERT INTO `flatanize_com`.`frequencies` (`id`, `description`) VALUES (DEFAULT, 'once');
+INSERT INTO `flatanize_com`.`frequencies` (`id`, `description`) VALUES (DEFAULT, 'daily');
+INSERT INTO `flatanize_com`.`frequencies` (`id`, `description`) VALUES (DEFAULT, 'weekly');
+INSERT INTO `flatanize_com`.`frequencies` (`id`, `description`) VALUES (DEFAULT, 'every month');
 
 COMMIT;
-
