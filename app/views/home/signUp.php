13a8dd4074b1c9d71eb894a9069f8a08e6f906ec
Nico <nico_florin@gmx.ch>
2015-07-29 21:00:36 +0200

    id form entfernt

--- app/views/home/signUp.php
+++ app/views/home/signUp.php
@@ -21,23 +21,23 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group <?php echo ($this->data['username'] === true ) ? 'has-error' : '' ?>">
-                            <input type="text" value="<?php echo (isset($this->data['username']) && $this->data['username'] !== true) ? $this->data['username'] : '' ?>" name="username" id="first_name" class="form-control input-lg" placeholder="User Name" tabindex="5" required autofocus>
+                            <input type="text" value="<?php echo (isset($this->data['username']) && $this->data['username'] !== true) ? $this->data['username'] : '' ?>" name="username" class="form-control input-lg" placeholder="User Name" tabindex="5" required autofocus>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group <?php echo ($this->data['displayname'] === true ) ? 'has-error' : '' ?>">
-                            <input type="text" value="<?php echo (isset($this->data['displayname']) && $this->data['displayname'] !== true) ? $this->data['displayname'] : '' ?>" name="displayname" id="last_name" class="form-control input-lg" placeholder="Display Name" tabindex="6" required>
+                            <input type="text" value="<?php echo (isset($this->data['displayname']) && $this->data['displayname'] !== true) ? $this->data['displayname'] : '' ?>" name="displayname" class="form-control input-lg" placeholder="Display Name" tabindex="6" required>
                         </div>
                     </div>
                 </div><!-- end row -->
                 <div class="form-group <?php echo ($this->data['email'] === true ) ? 'has-error' : '' ?>">
-                    <input type="email" value="<?php echo (isset($this->data['email']) && $this->data['email'] !== true) ? $this->data['email'] : '' ?>" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="7" required>
+                    <input type="email" value="<?php echo (isset($this->data['email']) && $this->data['email'] !== true) ? $this->data['email'] : '' ?>" name="email" class="form-control input-lg" placeholder="Email Address" tabindex="7" required>
                 </div>
                 <div class="form-group <?php echo ($this->data['password'] === true ) ? 'has-error' : '' ?>">
                     <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="8" required>
                 </div>
                 <div class="form-group">
-                    <input type="text" value="<?php echo (isset($this->data['flatCode']) && $this->data['flatCode'] !== true) ? $this->data['flatCode'] : '' ?>" name="flat_code" id="flat_code" class="form-control input-lg" placeholder="Flat Code (if available)" tabindex="9">
+                    <input type="text" value="<?php echo (isset($this->data['flatCode']) && $this->data['flatCode'] !== true) ? $this->data['flatCode'] : '' ?>" name="flat_code" class="form-control input-lg" placeholder="Flat Code (if available)" tabindex="9">
                 </div>
                 <div class="form-group">
                     <input type="submit" value="Sign Up" class="btn btn-success btn-block btn-lg" tabindex="10">
