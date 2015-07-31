0adcb65896ab61fec25a93db7d52ef63b155ccee
Nico <nico_florin@gmx.ch>
2015-07-31 16:53:20 +0200

    caret Settings dropdown

--- app/views/app/left_nav.php
+++ app/views/app/left_nav.php
@@ -5,8 +5,8 @@
         <li class="<?php echo ($this->data['title'] == 'Shopping List') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/shopping/index"><i class="fa fa-shopping-cart fa-fw fa-lg"></i> Shopping List</a></li>
         <li class="<?php echo ($this->data['title'] == 'Task Scheduling') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/task/index"><i class="fa fa-wrench fa-fw fa-lg"></i> Task Scheduling</a></li>
         <li class="<?php echo ($this->data['title'] == 'Finances') ? 'active' : '' ?>" <?php echo (!Session::getFlatId()) ? 'style="display:none;"' : '' ?>><a href="<?= URL ?>/finance/index" ><i class="fa fa-usd fa-fw fa-lg"></i> Finances</a></li>
-        <li id="NavSettings"><a><i class="fa fa-gears fa-fw fa-lg"></i> Settings</a></li>
-        <li class="<?php echo ($this->data['title'] == 'Flat Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/flatSettings"><i class="fa fa-building fa-fw fa-lg"></i> Flat Settings</a></li>
-        <li class="<?php echo ($this->data['title'] == 'User Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/userSettings"><i class="fa fa-street-view fa-fw fa-lg"></i> User Settings</a></li>
+        <li id="NavSettings"><a><i class="fa fa-gears fa-fw fa-lg"></i> Settings <b class="caret"></b></a></li>
+        <li class="<?php echo ($this->data['title'] == 'Flat Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/flatSettings" class="indent"><i class="fa fa-building fa-fw fa-lg"></i> Flat Settings</a></li>
+        <li class="<?php echo ($this->data['title'] == 'User Settings') ? 'active' : '' ?>"><a href="<?= URL ?>/settings/userSettings" class="indent"><i class="fa fa-street-view fa-fw fa-lg"></i> User Settings</a></li>
     </ul>
 </aside>
