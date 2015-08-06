67c96e1de78049a707e3ba2c8151162033df19a2
Nico <nico_florin@gmx.ch>
2015-08-06 20:28:08 +0200

    charset im string

--- app/libs/Database.php
+++ app/libs/Database.php
@@ -22,7 +22,8 @@ class Database extends PDO { //für extend PDO in php.ini: extension=php_pdo_mys
         $dsn = $settings['database']['driver'] .
                 ':host=' . $settings['database']['host'] .
                 ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
-                ';dbname=' . $settings['database']['db'];
+                ';dbname=' . $settings['database']['db'] .
+                ';charset=' . $settings['database']['charset'];
 
         // Erstelle PDO Objekt
         parent::__construct($dsn, $settings['database']['username'], $settings['database']['password']);
